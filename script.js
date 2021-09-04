b95c0d12bd7493b09938f0aae77ad3550f31b861
Pozender <66801105+Pozender@users.noreply.github.com>
2021-09-04 12:32:15 +0200

    old form solution

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const send = document.getElementById('envoyer');
+const form = document.querySelector('.formulaire');
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const formData = new FormData(this)
+
+
+
+    fetch('https://lecrayondorcontact.herokuapp.com/', {
+        method: 'POST',
+        body: formData
+    }).catch((error)=>{
+        console.error(error);
+    })
+})
+
+
+
+
+
     send.addEventListener('submit', () =>{
         alert("Votre message nous a bien été envoyé");
     })
